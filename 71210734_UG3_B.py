81db56f052d46e91148dfa6b6c2f5802cac0ca98
eunikeescd <93311869+eunikeescd@users.noreply.github.com>
2022-09-14 18:52:27 +0700

    Add files via upload

---
+++ 71210734_UG3_B.py
@@ -0,0 +1,33 @@
+#UG Strukdat_3
+randata = int(input("Masukkan range data:"))
+
+def fakt(n):
+   if n == 0 or n == 1:
+       return n
+   else:
+       return n*fakt(n-1)
+
+li = []
+for i in range (0,randata):
+    if i % 2 == 0:
+        li.append(i)
+    else:
+        continue
+
+faakt =[]
+for j in li:
+    if j % 2 == 0:
+        mam = fakt(j)
+        faakt.append(mam)
+    else:
+        continue
+
+dikt = dict(zip(li, faakt))
+
+print(dikt)
+
+cari = int(input("Data Ditampilkan:"))
+if cari in dikt:
+    print('Data ditemukan')
+else:
+    print('Data tidak ditemukan')
